ccbbcf68a5dae91da798513966c4f62e409ca53e
Buddy Galletti <buddygalletti@gmail.com>
2019-04-22 08:35:16 -0700

    Add files via upload
    Make sure to change the startDate and endDate stored in the url variable so that it will only collect the matchups you want.

---
+++ nhl_scrape.py
@@ -0,0 +1,50 @@
+# Owner: Buddy Galletti
+# Created: September 2018
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+import time
+from datetime import datetime
+import matplotlib.dates as mdates
+import matplotlib.ticker as ticker
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import csv
+import requests
+
+url = 'https://statsapi.web.nhl.com/api/v1/schedule?startDate=2018-10-03&endDate=2019-04-20'
+data = requests.get(url).json()
+
+download_dir = 'nhl_sched_2019.csv'
+csv = open(download_dir, "w")
+
+keys = data.keys()
+
+dates = data.get('dates')
+
+columnTitleRow = 'Date, Away, Home, My Pick, Winner, Score \n'
+csv.write(columnTitleRow)
+
+for date in dates:
+    games = date.get('games')
+    
+    for game in games:
+        teams = game.get('teams')
+        
+        home_team_name = teams['home']['team']['name']
+        away_team_name = teams['away']['team']['name']
+        
+        home_score = game['teams']['home']['score']
+        away_score = game['teams']['away']['score']
+        score = str(away_score) + '-' + str(home_score)
+
+        if (home_score > away_score):
+            winner = home_team_name
+        elif (home_score < away_score):
+            winner = away_team_name
+        else: winner = ''
+
+        row = date['date'] + ',' + away_team_name + ',' + home_team_name + ',,' + winner + ',' + score + '\n'
+        csv.write(row)
