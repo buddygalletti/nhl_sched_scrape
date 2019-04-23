75cc6e269c62495e1f367490b8ccc3107395c61c
Buddy Galletti <buddygalletti@gmail.com>
2019-04-23 10:17:57 -0700

    Update nhl_scrape.py

--- nhl_scrape.py
+++ nhl_scrape.py
@@ -14,17 +14,17 @@ from bs4 import BeautifulSoup
 import csv
 import requests
 
-url = 'https://statsapi.web.nhl.com/api/v1/schedule?startDate=2018-10-03&endDate=2019-04-20'
+url = 'https://statsapi.web.nhl.com/api/v1/schedule?startDate=2018-10-03&endDate=2019-04-20' #change startDate and endDate to whatever you prefer
 data = requests.get(url).json()
 
-download_dir = 'nhl_sched_2019.csv'
+download_dir = 'nhl_sched_2019.csv' #change file name to whatever you prefer
 csv = open(download_dir, "w")
 
 keys = data.keys()
 
 dates = data.get('dates')
 
-columnTitleRow = 'Date, Away, Home, My Pick, Winner, Score \n'
+columnTitleRow = 'Date, Away, Home, My Pick, Winner, Score \n' #change column titles to fit the data you want to collect
 csv.write(columnTitleRow)
 
 for date in dates:
